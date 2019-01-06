0ff9523177313d1da343d25e5e459ec6df73ae38
Justin Mauldin <justin.mauldin7@gmail.com>
2019-01-06 14:39:10 -0700

    Updates rating model test to account for Scorenly being from 1-5.

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Rating, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
     it { should validate_presence_of :score }
+    it { should validate_numericality_of(:score).only_integer }
+    it { should validate_numericality_of(:score).is_greater_than_or_equal_to(1) }
+    it { should validate_numericality_of(:score).is_less_than_or_equal_to(5) }
     it { should validate_presence_of :active }
   end
 
