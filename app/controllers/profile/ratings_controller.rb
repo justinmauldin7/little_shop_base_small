2333c3b40debd11b010cd5d62bba27e677806310
Justin Mauldin <justin.mauldin7@gmail.com>
2019-01-05 20:40:30 -0700

    Adds ratings_controller with code to create a rating successfully, and logic to handle successful creation & failed creation redirects and flash messages.

--- app/controllers/profile/ratings_controller.rb
+++ app/controllers/profile/ratings_controller.rb
@@ -6,11 +6,27 @@ class Profile::RatingsController < ApplicationController
     @form_path = [:profile, @order, @order_item, @rating]
   end
 
-  # def create
-  #   @rating = Rating.create(user: current_user)
-  #   binding.pry
-  #   redirect_to profile_order_path(params[:order_id])
-  #   flash[:success] = "Your item rating was created successfully"
-  # end
+  def create
+    @item = OrderItem.find(params[:order_item_id]).item
+    @rating = Rating.create(item: @item,
+                            user: current_user,
+                            title: rating_params[:title],
+                            description: rating_params[:description],
+                            score: rating_params[:score],
+                            active: true)
 
+    if @rating.save
+      redirect_to profile_order_path(params[:order_id])
+      flash[:success] = "Your rating was created successfully."
+    else
+      flash[:error] = "Your rating has not been created."
+      render :new
+    end
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:title, :description, :score)
+  end
 end
